d44e966bb6ce8f6b374462fa01579445edcc1135
Jamshid <mamatovjamshid506@gmail.com>
2021-02-26 20:20:54 +0500

    information about request

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from flask import Flask,request
 
-app = Flask(__name__)
+app=Flask(__name__)
 
+@app.route("/")
 
-@app.route('/api')
-def api():
-    r = request.args
-    a=r['a']
-    b=r['b']
-    return {'a':a,'b':b}
+def request_inform():
+    
+    path=request.path
+    full_path=request.full_path
+    script_root=request.script_root
+    base_url=request.base_url
+    url=request.url
 
+    inform=f"""
+    <p>path:{path}</p>
+    <p>full_path:{full_path}</p>
+    <p>script_root:{script_root}</p>
+    <p>base_url:{base_url}</p>
+    <p>url:{url}</p>
+
+    """
+
+    return inform
 
 app.run(debug=True)
