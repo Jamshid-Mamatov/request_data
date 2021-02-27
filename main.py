c53f62e573ccb14c00ab6cba51b0be2b3cc98ca0
Jamshid <mamatovjamshid506@gmail.com>
2021-02-27 10:45:00 +0500

    args.items

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def api():
     r=request.args
     print(r)
     # print(request.url)
+    
     a=int(r.get("a",0))
     b=int(r.get("b",0))
 
+    for k,v in r.items():
+        print(k,v)
     return {"result":a+b}
 
 app.run(debug=True)
