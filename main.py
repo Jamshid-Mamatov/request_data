7c7bbed36b7c969b94f20e29e13458aafb29ff95
Jamshid <mamatovjamshid506@gmail.com>
2021-02-27 08:31:03 +0500

    request args

--- main.py
+++ main.py
@@ -4,23 +4,11 @@ app=Flask(__name__)
 
 @app.route("/")
 
-def request_inform():
-    
-    path=request.path
-    full_path=request.full_path
-    script_root=request.script_root
-    base_url=request.base_url
-    url=request.url
+def api():
+    r=request.args
+    print(r)
+    print(request.url)
 
-    inform=f"""
-    <p>path:{path}</p>
-    <p>full_path:{full_path}</p>
-    <p>script_root:{script_root}</p>
-    <p>base_url:{base_url}</p>
-    <p>url:{url}</p>
-
-    """
-
-    return inform
+    return "ok"
 
 app.run(debug=True)
