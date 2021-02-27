dcc566881af0a293f0321c5b54139620e6b7d79d
Jamshid <mamatovjamshid506@gmail.com>
2021-02-27 08:48:48 +0500

    query request

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ app=Flask(__name__)
 @app.route("/")
 
 def api():
+
     r=request.args
     print(r)
-    print(request.url)
+    # print(request.url)
+    a=int(r['a'])
+    b=int(r['b'])
 
-    return "ok"
+    return {"result":a+b}
 
 app.run(debug=True)
