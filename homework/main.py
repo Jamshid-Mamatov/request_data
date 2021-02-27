189b083244ed58ee843f787474b6395849fa7140
Jamshid <mamatovjamshid506@gmail.com>
2021-02-27 20:54:07 +0500

    with tinydb database created

--- homework/main.py
+++ homework/main.py
@@ -1,9 +1,24 @@
 from logging import debug
 from flask import Flask,request
-from tinydb import TinyDB
+from tinydb import TinyDB,Query
+import json
+
 
 app=Flask(__name__)
 
+
+q=Query()
+
+file=open("country_by_capital_city.json").read()
+
+f=json.loads(file)
+
+db=TinyDB("database.json")
+
+table1=db.table("inform country and city")
+
+table1.truncate()
+table1.insert_multiple(f)
 
 @app.route("/api",methods=["POST","GET"])
 def api():
@@ -19,6 +34,15 @@ def api():
     return "ok"
 
 
+
+
+def database(country:str)->dict:
+    pass
+
+
+
 app.run(debug=True)
+
+
 
 
