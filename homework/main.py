d1c3d6d11b01038f9a5f462e797bd48b1fe490a9
Jamshid <mamatovjamshid506@gmail.com>
2021-02-27 20:22:02 +0500

    request method

--- homework/main.py
+++ homework/main.py
@@ -1,14 +1,24 @@
 from logging import debug
 from flask import Flask,request
+from tinydb import TinyDB
 
 app=Flask(__name__)
 
 
-@app.route("/api",methods=["POST"])
+@app.route("/api",methods=["POST","GET"])
 def api():
     if request.method=="POST":
-        return {}
+        r=request.form
+        country=r.get("country","")
+        country=country.capitalize()
+        # print(r)
+
+
+        return "ok"
+    
     return "ok"
 
 
 app.run(debug=True)
+
+
